27a513bd1ffee055bba59868a9c9debf27eb4f71
ItayElf <thh123321@gmail.com>
2024-07-19 12:55:48 +0300

    Feat: added randpg

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { Notice, Plugin } from "obsidian";
+import randpg from "./randpg/randpg";
 
 export default class RealmRoller extends Plugin {
 	async onload() {
-		console.log("test");
+		console.log(randpg.generateWorld());
 		this.addRibbonIcon("dice", "Greet", () => {
 			new Notice("Hello, world!");
 		});
