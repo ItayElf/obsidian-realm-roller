3066f9d7f2770d957410352e70c46521d0198a8b
ItayElf <thh123321@gmail.com>
2024-07-19 15:35:12 +0300

    Feat: added name generation

---
+++ src/commands/generateNames.ts
@@ -0,0 +1,26 @@
+import { App } from "obsidian";
+import randpg from "../randpg/randpg";
+import OptionalModal from "src/modals/optionalModal";
+import NamesModal from "src/modals/namesModal";
+import { getRaces } from "./generateNpc";
+
+const onGenerate = async (app: App, type: string | null) => {
+	const names = randpg.generateNames(type?.toLowerCase());
+	new NamesModal(app, `${type ?? "Random"} Names`, names, () =>
+		randpg.generateNames(type?.toLowerCase())
+	).open();
+};
+
+const getNamesCommand = (app: App) => {
+	return {
+		id: "realm-roller-generate-names",
+		name: "Generate Names",
+		callback: () => {
+			new OptionalModal(app, getRaces(), (r) =>
+				onGenerate(app, r)
+			).open();
+		},
+	};
+};
+
+export default getNamesCommand;
--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import { Plugin } from "obsidian";
 import getCompanionCommand from "./commands/generateCompanion";
 import getCompanionNamesCommand from "./commands/generateCompanionNames";
 import getNpcCommand from "./commands/generateNpc";
+import getNamesCommand from "./commands/generateNames";
 
 export default class RealmRoller extends Plugin {
 	async onload() {
 		this.addCommand(getCompanionCommand(this.app));
 		this.addCommand(getCompanionNamesCommand(this.app));
 		this.addCommand(getNpcCommand(this.app));
+		this.addCommand(getNamesCommand(this.app));
 	}
 }
